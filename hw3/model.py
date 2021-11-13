cf41e4e233e08c9a93a60eee06df27319116f7d9
NeedaBluePrint <75880630+Dong-Hwa-Lee@users.noreply.github.com>
2021-11-13 18:10:26 +0900

    Update model.py

--- hw3/model.py
+++ hw3/model.py
@@ -96,10 +96,10 @@ class Transcriber_RNN(nn.Module):
         # Notice: Changing the initialization order may fail the tests.
         self.melspectrogram = LogMelSpectrogram()
 
-        self.frame_lstm = nn.LSTM(input_size=N_MELS, hidden_size=cnn_unit, num_layers=2, bidirectional=True)
+        self.frame_lstm = nn.LSTM(input_size=N_MELS, hidden_size=cnn_unit, num_layers=2, batch_first=True, bidirectional=True)
         self.frame_fc = nn.Linear(fc_unit, 88)
 
-        self.onset_lstm = nn.LSTM(input_size=N_MELS, hidden_size=cnn_unit, num_layers=2, bidirectional=True)
+        self.onset_lstm = nn.LSTM(input_size=N_MELS, hidden_size=cnn_unit, num_layers=2, batch_first=True, bidirectional=True)
         self.onset_fc = nn.Linear(fc_unit, 88)
 
     def forward(self, audio):
@@ -125,11 +125,11 @@ class Transcriber_CRNN(nn.Module):
         self.melspectrogram = LogMelSpectrogram()
 
         self.frame_conv_stack = ConvStack(N_MELS, cnn_unit, fc_unit)
-        self.frame_lstm = nn.LSTM(input_size=N_MELS, hidden_size=cnn_unit, num_layers=2, bidirectional=True)
+        self.frame_lstm = nn.LSTM(input_size=N_MELS, hidden_size=cnn_unit, num_layers=2, batch_first=True, bidirectional=True)
         self.frame_fc = nn.Linear(fc_unit, 88)
 
         self.onset_conv_stack = ConvStack(N_MELS, cnn_unit, fc_unit)
-        self.onset_lstm = nn.LSTM(input_size=N_MELS, hidden_size=cnn_unit, num_layers=2, bidirectional=True)
+        self.onset_lstm = nn.LSTM(input_size=N_MELS, hidden_size=cnn_unit, num_layers=2, batch_first=True, bidirectional=True)
         self.onset_fc = nn.Linear(fc_unit, 88)
 
     def forward(self, audio):
@@ -160,10 +160,10 @@ class Transcriber_ONF(nn.Module):
         self.frame_fc = nn.Linear(fc_unit, 88)
 
         self.onset_conv_stack = ConvStack(N_MELS, cnn_unit, fc_unit)
-        self.onset_lstm = nn.LSTM(input_size=N_MELS, hidden_size=cnn_unit, num_layers=2, bidirectional=True)
+        self.onset_lstm = nn.LSTM(input_size=N_MELS, hidden_size=cnn_unit, num_layers=2, batch_first=True, bidirectional=True)
         self.onset_fc = nn.Linear(fc_unit, 88)
 
-        self.combined_lstm = nn.LSTM(input_size=N_MELS, hidden_size=cnn_unit, num_layers=2, bidirectional=True)
+        self.combined_lstm = nn.LSTM(input_size=N_MELS, hidden_size=cnn_unit, num_layers=2, batch_first=True, bidirectional=True)
         self.combined_fc = nn.Linear(fc_unit, 88)
 
     def forward(self, audio):
